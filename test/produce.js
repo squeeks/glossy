0ec131c8b1da19bb84833aedeffeaa14f46ae559
Squeeks <privacymyass@gmail.com>
2011-02-09 16:25:26 +0000

    Adding callback test to produce test.

--- test/produce.js
+++ test/produce.js
@@ -20,3 +20,17 @@ var msg = syslogProducer.produce({
 });
 
 assert.equal(msg, "<163>1 2009-01-13T23:31:30.00Z localhost sudo 123 - Test Message",'Valid message returned');
+
+syslogProducer.produce({
+	facility: 'audit',
+	severity: 'error',
+	host: '127.0.0.1',
+	app_id: 'sudo',
+	pid: '419',
+	date: new Date(1234567890000),
+	message: 'Test Message'
+}, function(cbMsg) {
+	assert.equal(cbMsg, '<107>1 2009-01-13T23:31:30.00Z 127.0.0.1 sudo 419 - Test Message', 'Valid message in callback returned');
+});
+
+
